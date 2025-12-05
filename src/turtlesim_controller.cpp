f21731afe4965bd91a0d72c44cc99926f63add16
CarmineD8 <carmine.recchiuto@dibris.unige.it>
2025-12-05 10:08:41 +0000

    update

--- src/turtlesim_controller.cpp
+++ src/turtlesim_controller.cpp
@@ -10,8 +10,8 @@ class TurtlesimController: public rclcpp::Node
 public:
 TurtlesimController(): Node("turtlesim_controller")
  {
- subscription_ = this->create_subscription<turtlesim::msg::Pose>("turtle2/pose", 10, std::bind(&TurtlesimController::topic_callback, this, _1));
- publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle2/cmd_vel", 10);
+ subscription_ = this->create_subscription<turtlesim::msg::Pose>("turtle1/pose", 10, std::bind(&TurtlesimController::topic_callback, this, _1));
+ publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
  publisher_vel_ = this->create_publisher<turtlesim_custom_msgs::msg::Vel>("vel_topic", 10);
  timer_ = this->create_wall_timer(std::chrono::milliseconds(50), std::bind(&TurtlesimController::timer_callback, this));
  }
